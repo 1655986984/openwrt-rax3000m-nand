de898beec26a1151bb32fd839096e7a0ad697165
1655986984 <1655986984@qq.com>
2024-04-25 01:49:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,16 +11,16 @@
 #
 # 通过修改diy-part2.sh文件可以自定义默认IP，登陆密码等。按我的需要现在的默认IP为172.16.1.1
 # 修改openwrt登陆地址,把下面的172.16.1.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.138.1.1/g' ./package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' ./package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/immortalwrt/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/RAX3000M/g' ./package/base-files/files/bin/config_generate
 
 # Enable wifi
 # sed -i 's/.disabled=1/.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # echo "修改wifi名称"
-# sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i "s/OpenWrt/$wifi_CMCC/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Set Wifi SSID and Password
 # sed -i 's/.ssid=OpenWrt/.ssid=Tomato24/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -32,7 +32,4 @@ sed -i 's/OpenWrt/immortalwrt/g' ./package/base-files/files/bin/config_generate
 
 # Set default root password
 # sed -i 's/root::0:0:99999:7:::/root:$1$kWRCl0Y2$7JL\/jLAF1xoVIiIMdTO5f.:16788:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# 修改默认主题Modify default THEME
-# sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial_new/g' ./feeds/luci/collections/luci/Makefile
 
